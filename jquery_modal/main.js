74868cfe09bb7b0104b34999dcbcc2d58ebc2b00
reynazuno <364081@colum.edu>
2020-11-17 23:01:59 -0600

    jQuery

--- jquery_modal/main.js
+++ jquery_modal/main.js
@@ -1,7 +1,9 @@
-$(".open").on("click", function() {
+$(".open").on("click", function() 
+{
     $(".popup-overlay, .popup-content").addClass("active");
-  });
-  
-  $(".close, .popup-overlay").on("click", function() {
+});
+
+$(".close, .popup-overlay").on("click", function() 
+{
     $(".popup-overlay, .popup-content").removeClass("active");
-  });
+});
